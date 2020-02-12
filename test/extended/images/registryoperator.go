d0e447068d89be9f62d8eb071aa78c6c052055a3
wzheng <wzheng@redhat.com>
2020-02-12 16:24:19 +0800

    test

--- test/extended/images/registryoperator.go
+++ test/extended/images/registryoperator.go
@@ -52,13 +52,15 @@ func EnsureRegistryOperatorStatusIsAvailable(oc *exutil.CLI) {
 
 // NewClientset creates a set of Kubernetes clients. The default kubeconfig is
 // used if not provided.
-func NewClientset(kubeconfig *restclient.Config) (clientset *Clientset, err error) {
+func NewClientset() (clientset *Clientset, err error) {
 	/*if kubeconfig == nil {
 		kubeconfig, err = client.GetConfig()
 		if err != nil {
 			return nil, fmt.Errorf("unable to get kubeconfig: %s", err)
 		}
 	}*/
+	kubeconfig, err := e2e.LoadConfig()
+	o.Expect(err).ToNot(o.HaveOccurred())
 
 	clientset = &Clientset{}
 	clientset.CoreV1Interface, err = clientcorev1.NewForConfig(kubeconfig)
@@ -91,7 +93,7 @@ func NewClientset(kubeconfig *restclient.Config) (clientset *Clientset, err erro
 // MustNewClientset is like NewClienset but aborts the test if clienset cannot
 // be constructed.
 func MustNewClientset(kubeconfig *restclient.Config) *Clientset {
-	clientset, err := NewClientset(kubeconfig)
+	clientset, err := NewClientset()
 	if err != nil {
 		e2e.Logf("Cannot get kubeconfig")
 	}
